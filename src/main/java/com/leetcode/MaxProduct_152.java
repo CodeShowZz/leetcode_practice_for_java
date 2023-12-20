7fc9a9933b627a6fa7e892470fdd4427d9107c0b
doudou <agilebeanisartist@gmail.com>
2023-12-20 23:33:33 +0800

    添加两题

---
+++ src/main/java/com/leetcode/MaxProduct_152.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+/**
+ * @author lin
+ * @date 2023/12/20 22:58
+ **/
+public class MaxProduct_152 {
+
+    public int maxProduct(int[] nums) {
+        if(nums ==null || nums.length == 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int tempMin = 1;
+        int tempMax = 1;
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(nums[i] < 0) {
+                int temp = tempMax;
+                tempMax = tempMin;
+                tempMin = temp;
+            }
+            tempMax = Math.max(nums[i],nums[i] * tempMax);
+            tempMin = Math.min(nums[i],nums[i] * tempMin);
+            max = Math.max(tempMax,max);
+        }
+        return max;
+    }
+}
---
+++ src/main/java/com/leetcode/Rob_198.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+/**
+ * @author lin
+ * @date 2023/12/20 23:05
+ **/
+public class Rob_198 {
+
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int [] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int k = 2;k <=n;k++) {
+            dp[k] = Math.max(dp[k-1],nums[k-1] + dp[k-2]);
+        }
+        return dp[n];
+    }
+}
