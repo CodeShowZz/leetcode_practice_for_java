de069a0b8665ec29f91ad881cc07f4747411e788
lin <linhuang@yeahka.com>
2023-12-25 22:14:58 +0800

    leetcode

---
+++ src/main/java/com/leetcode/HammingDistance_461.java
@@ -0,0 +1,12 @@
+package com.leetcode;/**
+ *
+ * @description:
+ * @author: Linhuang
+ * @date: 2023-12-25 19:28
+ */
+public class HammingDistance_461 {
+
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
---
+++ src/main/java/com/leetcode/MoveZeroes_283.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+import com.leetcode.util.ArrayUtil;
+
+/**
+ *
+ * @description:
+ * @author: Linhuang
+ * @date: 2023-12-25 19:15
+ */
+public class MoveZeroes_283 {
+
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return;
+        }
+        int len = nums.length;
+        for(int i = 1;i<len;i++) {
+            for(int j = i;j > 0;j--) {
+                 if(nums[j] != 0 && nums[j-1] ==0) {
+                     ArrayUtil.swap(nums,j,j-1);
+                 } else {
+                     break;
+                 }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {0,1,0,3,12};
+        new MoveZeroes_283().moveZeroes(nums);
+    }
+
+
+}
