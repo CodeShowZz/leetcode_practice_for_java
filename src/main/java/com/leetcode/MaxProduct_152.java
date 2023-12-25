8ed6a017cfa5d2d3d17954237f13c534060ac51f
lin <linhuang@yeahka.com>
2023-12-25 15:09:15 +0800

    fix

--- src/main/java/com/leetcode/MaxProduct_152.java
+++ src/main/java/com/leetcode/MaxProduct_152.java
@@ -1,22 +1,6 @@
 package com.leetcode;
 
-<<<<<<< HEAD
-import com.leetcode.util.ArrayUtil;
 
-/**
- * @description:
- * @author: Linhuang
- * @date: 2023-12-20 18:07
- */
-public class MaxProduct_152 {
-
-    public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int tempMax = 1;
-        int tempMin = 1;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-=======
 /**
  * @author lin
  * @date 2023/12/20 22:58
@@ -24,22 +8,20 @@ public class MaxProduct_152 {
 public class MaxProduct_152 {
 
     public int maxProduct(int[] nums) {
-        if(nums ==null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int max = Integer.MIN_VALUE;
         int tempMin = 1;
         int tempMax = 1;
-        for(int i = 0 ; i < nums.length ; i++) {
->>>>>>> 7fc9a9933b627a6fa7e892470fdd4427d9107c0b
-            if(nums[i] < 0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
                 int temp = tempMax;
                 tempMax = tempMin;
                 tempMin = temp;
             }
-<<<<<<< HEAD
-            tempMax = Math.max(tempMax,tempMax*nums[i]);
-            tempMin = Math.min(tempMin,tempMin*nums[i]);
+            tempMax = Math.max(tempMax, tempMax * nums[i]);
+            tempMin = Math.min(tempMin, tempMin * nums[i]);
         }
         return max;
     }
@@ -49,12 +31,3 @@ public class MaxProduct_152 {
         int res = new MaxProduct_152().maxProduct(nums);
     }
 }
-=======
-            tempMax = Math.max(nums[i],nums[i] * tempMax);
-            tempMin = Math.min(nums[i],nums[i] * tempMin);
-            max = Math.max(tempMax,max);
-        }
-        return max;
-    }
-}
->>>>>>> 7fc9a9933b627a6fa7e892470fdd4427d9107c0b
