169772e327fec10a35f737014422966e309fcf58
lin <linhuang@yeahka.com>
2023-12-29 18:09:09 +0800

    commit two problem

---
+++ src/main/java/com/leetcode/DailyTemperatures_739.java
@@ -0,0 +1,55 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * @description:
+ * @author: Linhuang
+ * @date: 2023-12-29 14:44
+ */
+public class DailyTemperatures_739 {
+
+    /**
+     * 示例 1:
+     * <p>
+     * 输入: temperatures = [73,74,75,71,69,72,76,73]
+     * <p>
+     * <p>
+     * <p>
+     * 输出: [1,1,4,2,1,1,0,0]
+     *
+     * @param temperatures
+     * @return
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        if (temperatures == null || temperatures.length == 0) {
+            return temperatures;
+        }
+        LinkedList<Integer> temperaturesStack = new LinkedList<>();
+        int len = temperatures.length;
+        int []res= new int[len];
+        for (int i = 0; i < len; i++) {
+            if (temperaturesStack.size() == 0) {
+                temperaturesStack.push(i);
+            } else {
+                while (temperaturesStack.size() != 0) {
+                    int peek = temperaturesStack.peek();
+                    if (temperatures[i] > temperatures[peek]) {
+                        int index = temperaturesStack.pop();
+                        res[index] = i - peek;
+                    } else {
+                        break;
+                    }
+                }
+                temperaturesStack.push(i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] res = new DailyTemperatures_739().dailyTemperatures(temperatures);
+        System.out.println(Arrays.toString(res));
+    }
+}
---
+++ src/main/java/com/leetcode/LowestCommonAncestor_236.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import com.leetcode.model.TreeNode;
+
+/**
+ *
+ * @description:
+ * @author: Linhuang
+ * @date: 2023-12-29 17:12
+ */
+public class LowestCommonAncestor_236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) {
+            return null;
+        } else if(left == null && right != null) {
+            return right;
+        } else if(left != null && right == null) {
+            return left;
+        } else if(left != null && right != null) {
+            return root;
+        }
+        return root;
+    }
+
+
+
+
+}
